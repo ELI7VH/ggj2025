8f6afbed23bfec193f352f6204f4eaca5aea6b7b
leedavey <lee.davey@gmail.com>
2025-01-24 20:46:03 -0700

    adding physics bubble process

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -5,8 +5,10 @@ extends RigidBody2D
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _physics_process(delta: float) -> void:
+	position.y = position.y - 1
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y = position.y - 1
 	pass
