cc61bdd7a6075e32403e060ab5edfd3a855038f2
leedavey <lee.davey@gmail.com>
2025-01-25 12:15:42 -0700

    First puzzle

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 @export var floatup = Vector2(0,-200)
 @export var sideDecel = 0.7
+@export var timetolive = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,6 +16,7 @@ func _physics_process(delta: float) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# remove bubble if it goes offscreen
-	if (position.y < 0):
+	timetolive = timetolive - delta
+	if (position.y < 0) or (timetolive < 0):
 		queue_free()
 	pass
