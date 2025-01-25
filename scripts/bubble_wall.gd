6872032e5bea0c20145a74b7e6c1d75d7a397149
leedavey <lee.davey@gmail.com>
2025-01-24 23:45:14 -0700

    Adding lever

--- scripts/bubble_wall.gd
+++ scripts/bubble_wall.gd
@@ -14,7 +14,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if count > emit_frequency:
-		print(delta)
 		var newBubble = bubbleScene.instantiate()
 		newBubble.position = position
 		newBubble.position.x = position.x + randi_range(-side_jitter,side_jitter)
