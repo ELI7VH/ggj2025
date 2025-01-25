7ead2c74eadad0429d84c4327f4a4552eb5ea915
leedavey <lee.davey@gmail.com>
2025-01-24 21:08:59 -0700

    changing bubble physics

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -1,12 +1,14 @@
 extends RigidBody2D
 
+var floatup = Vector2(0,-200)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _physics_process(delta: float) -> void:
-	position.y = position.y - 1
+	apply_central_force(floatup)	
+#	move_and_collide(floatup)
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
