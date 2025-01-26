86732adc197386bf5d357791ae10d2deba52b939
elijah <elijahlucian@gmail.com>
2025-01-26 12:53:17 -0700

    chargeybreath sound

--- scripts/audio/bubble_charge.gd
+++ scripts/audio/bubble_charge.gd
@@ -1,5 +1,8 @@
 extends AudioStreamPlayer
 
+@export var bubble_blower: BubbleBlower
+
+var t = 0.0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,14 +11,25 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not playing:
+		return
+	
+	var breath_expended = bubble_blower.held_bubble_scale
+
+	var pitch = 1.0 + breath_expended * 0.5
+	t += delta
+	var mod = sin(t * TAU * 4) * 0.05
+	pitch_scale = pitch + mod
 
 func _on_bubble_blower_bubble_charging() -> void:
 	play()
 
-func _on_bubble_blower_bubble_blown(breath_expended: float) -> void:
+func stop_breath() -> void:
 	stop()
+	t = 0
+
+func _on_bubble_blower_bubble_blown(_breath_expended: float) -> void:
+	stop_breath()
 
 func _on_bubble_blower_breath_exhausted() -> void:
-	print('should stop')
-	stop()
+	stop_breath()
