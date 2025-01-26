154c1532e449c3e514dd5d22b6b0d7f8b81bef4c
Twig Nielsen <littleb88888888@gmail.com>
2025-01-25 19:28:30 -0700

    Implemented variable buoyancy - fixed default value of buoyancy enabled flag - made bubbles scale buoyancy with size - added frictionless bouncy physics material to bubble

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -3,12 +3,13 @@ extends RigidBody2D
 
 const GROUP_NAME = 'bubbles'
 
-@export var buoyancy: float = 200
+@export var base_buoyancy: float = 200
+@export var buoyancy_reference_scale: float = 10
 @export var collision_shape: CollisionShape2D
 @export var drawer: BubbleDrawer
 
 var radius: float = 8: set = set_radius
-var buoyancy_enabled: bool = false: set = set_physics_enabled
+var buoyancy_enabled: bool = true: set = set_buoyancy_enabled
 
 var collider: CircleShape2D
 
@@ -22,6 +23,8 @@ func _ready():
 
 func _physics_process(_delta: float) -> void:
 	if buoyancy_enabled:
+		var buoyancy_scale = radius / buoyancy_reference_scale
+		var buoyancy = base_buoyancy * buoyancy_scale
 		apply_central_force(Vector2.UP * buoyancy)
 
 func _process(_delta: float) -> void:
@@ -36,6 +39,6 @@ func set_radius(value: float):
 	if collider:
 		collider.radius = value
 
-func set_physics_enabled(value: bool):
+func set_buoyancy_enabled(value: bool):
 	buoyancy_enabled = value
 	collision_layer = 1 if value else 0
