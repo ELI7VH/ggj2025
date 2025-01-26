9514dd2d10ba60ae9eceb39a08746a283aeb61e6
Twig Nielsen <littleb88888888@gmail.com>
2025-01-26 12:10:14 -0700

    Added volume slider to start menu

--- scripts/game_management/menu_manager.gd
+++ scripts/game_management/menu_manager.gd
@@ -1,15 +1,25 @@
 extends Control
 
-@export var main_menu_root: Control
+@export var start_menu_root: Control
 @export var level_manager: LevelProgressionManager
+@export var volume_slider: Slider
+
+
+func _ready() -> void:
+	var master_bus_index = AudioServer.get_bus_index('Master')
+	volume_slider.value = db_to_linear(AudioServer.get_bus_volume_db(master_bus_index))
+	volume_slider.value_changed.connect(_set_master_volume)
 
 
 func start_game():
-	main_menu_root.hide()
+	start_menu_root.hide()
 	level_manager.load_starting_level()
 
+func show_start_menu():
+	start_menu_root.show()
+
 func show_level_select_screen():
-	pass
+	start_menu_root.hide()
 
 func quit():
 	get_tree().quit()
@@ -17,3 +27,8 @@ func quit():
 
 func toggle_pause():
 	get_tree().paused = !get_tree().paused
+
+
+func _set_master_volume(value: float):
+	var master_bus_index = AudioServer.get_bus_index('Master')
+	AudioServer.set_bus_volume_db(master_bus_index, linear_to_db(value))
