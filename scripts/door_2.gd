3ba2b97582956edc1dc0dda9739e97155aa16c9b
leedavey <lee.davey@gmail.com>
2025-01-25 19:58:27 -0700

    Using const force

--- scripts/door_2.gd
+++ scripts/door_2.gd
@@ -7,7 +7,8 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _physics_process(delta: float) -> void:
-	apply_central_force(gravity*20)
+#	apply_central_force(gravity*20)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
