027f1cf425ce81422aa953a12240a63280849913
Twig Nielsen <littleb88888888@gmail.com>
2025-01-25 23:43:55 -0700

    Implemented bubble receptacle

---
+++ scripts/bubble_receptacle.gd
@@ -0,0 +1,38 @@
+class_name BubbleReceptacle
+extends RigidBody2D
+
+signal bubble_added(current_progress: float)
+signal threshold_reached
+
+@export var height_threshold: float = 42
+
+@export var fill_area: Area2D
+@export var screw: Sprite2D
+@export var animator: AnimationPlayer
+
+@onready var rest_height: float = position.y
+@onready var screw_rest_height: float = screw.region_rect.size.y
+
+var listening_for_threshold = true
+
+
+func _physics_process(_delta: float) -> void:
+	var screw_rect = Rect2(screw.region_rect)
+	var y_offset = position.y - rest_height
+	screw_rect.position.y = -y_offset
+	screw_rect.size.y = screw_rest_height + y_offset * 2
+	screw.region_rect = screw_rect
+
+	if position.y < height_threshold && listening_for_threshold:
+		listening_for_threshold = false
+		animator.play('spin')
+		threshold_reached.emit();
+
+
+func _on_body_entered_fill_area(body: PhysicsBody2D):
+	if body is Bubble && listening_for_threshold:
+		bubble_added.emit(get_normalized_progress())
+
+
+func get_normalized_progress() -> float:
+	return remap(position.y, rest_height, height_threshold, 0, 1)
